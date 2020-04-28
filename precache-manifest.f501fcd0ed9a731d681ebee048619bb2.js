6bac32a37433b20ab14949250740fa1f451cc35c
zxb <121179422@qq.com>
2020-04-28 17:51:42 +0800

    打包项目

---
+++ precache-manifest.f501fcd0ed9a731d681ebee048619bb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8e4a3c220cb2119f0880fef891d673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd6cf4bc6c5b435cc723",
+    "url": "/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "2f1f9a5fabf5ed70026d",
+    "url": "/static/css/main.dca85782.chunk.css"
+  },
+  {
+    "revision": "dd6cf4bc6c5b435cc723",
+    "url": "/static/js/2.f4917752.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f4917752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1f9a5fabf5ed70026d",
+    "url": "/static/js/main.4f0675f0.chunk.js"
+  },
+  {
+    "revision": "a424972292514c0f9109",
+    "url": "/static/js/runtime-main.f13d9088.js"
+  },
+  {
+    "revision": "c187f82e38cbe167b4f581c3683ec001",
+    "url": "/static/media/logo.c187f82e.png"
+  }
+]);
