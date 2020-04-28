a13ecf0386c70ec6fb8826427a6b59b66c9e6d46
zxb <121179422@qq.com>
2020-04-28 17:36:17 +0800

    增加列表效果地址

--- src/page/list/index.jsx
+++ src/page/list/index.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
-
-export default class List extends React.Component{
-    render() {
-        return (
-            <div>
-                List
-                <iframe ref={(ref)=>{this.iframe = ref}}  className="iframe" title="xxx" src="http://www.imagehover.io/"  frameBorder="0"></iframe>
-            </div>
-        );
-    }
+import Slider from '../../components/slider';
+import { Layout } from 'antd';
+const { Content } = Layout;
+export default class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuList: ['效果一', '效果二', '效果三'],
+      curIndex: 0
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Layout className="site-layout-background">
+          <Slider
+            menuList={this.state.menuList}
+            handlerCick={this.handlerCick}
+          ></Slider>
+          <Content className="site-layout-background">
+           
+              <iframe
+                ref={(ref) => {
+                  this.iframe = ref;
+                        }}
+                        id="iframe"
+                className="iframe"
+                title="xxx"
+                src="http://www.imagehover.io/"
+                frameBorder="0"
+              ></iframe>
+            {this.state.curIndex === 1 && (
+              <iframe
+                
+               
+                className="iframe"
+                title="xxx"
+                src="http://hasinhayder.github.io/ImageCaptionHoverAnimation/index2.html"
+                frameBorder="0"
+              ></iframe>
+            )}
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
+  handlerCick = (item, index) => {
+    console.log(item);
+    this.setState({
+      curIndex: index
+    });
+  };
 }
