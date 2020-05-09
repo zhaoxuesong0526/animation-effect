4413172a2bdb9b32efa3ab6b77dc7ee9aac9915f
qianll <qll19930105@163.com>
2020-05-09 16:58:36 +0800

    添加slider效果二

--- src/page/slider/index.jsx
+++ src/page/slider/index.jsx
@@ -28,23 +28,19 @@ export default class Image extends React.Component {
                   id="iframe"
                   className="iframe"
                   title="xxx"
-                  src="https://tympanus.net/Development/HoverEffectIdeas/"
+                  src=""
                   frameBorder="0"
                 ></iframe>
               </div>
             )}
             {this.state.curIndex === 1 && (
               <div>
-                <iframe
-                  ref={(ref) => {
-                    this.iframe = ref;
-                  }}
-                  id="iframe"
-                  className="iframe"
-                  title="xxx"
-                  src="https://codepen.io/AdamCCFC/details/WvzBKq"
-                  frameBorder="0"
-                ></iframe>
+                <a style={{ display: 'block'}} target="_blank" href="https://www.jssor.com/demos/nearby-image-partial-visible-slider.slider">https://www.jssor.com/demos/nearby-image-partial-visible-slider.slider</a>
+                <a style={{ display: 'block'}} target="_blank" href="https://www.jssor.com/demos/scrolling-logo-thumbnail-slider.slider">https://www.jssor.com/demos/scrolling-logo-thumbnail-slider.slider</a>
+                <a style={{ display: 'block' }} target="_blank" href="https://www.jssor.com/demos/image-gallery-with-vertical-thumbnail.slider">https://www.jssor.com/demos/image-gallery-with-vertical-thumbnail.slider</a>
+                <a style={{ display: 'block' }} target="_blank" href="https://www.jssor.com/slideshow/x-zoom.html">https://www.jssor.com/slideshow/x-zoom.html</a>
+                <a style={{ display: 'block' }} target="_blank" href="https://www.jssor.com/skins/thumbnail/thumbnail-skin-121.slider/=skin">https://www.jssor.com/skins/thumbnail/thumbnail-skin-121.slider/=skin</a>
+                            
               </div>
             )}
           </Content>
